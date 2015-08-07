497a318ee14ccc20c2e600a30c7d4c247a339026
justinweathersby <justinweathersby@gmail.com>
2015-08-07 14:12:38 -0400

    Adding new services

--- www/js/services.js
+++ www/js/services.js
@@ -1,27 +1,33 @@
-app.service('authService', function($http){
+app.service('authService', function($http, currentUserService){
   this.login = function(user){
-    return  $http({method: 'POST', url: '/api', headers: {'X-API-EMAIL' : user.email, 'X-API-PASS' : user.password}})
+    return  $http({method: 'POST', url: '/api/login', headers: {'X-API-EMAIL' : user.email, 'X-API-PASS' : user.password}})
       .success( function( data )
       {
-        success(data);
-      })
-      .error( function( data)
-      {
-      error(data);
-      }); //--End of return statement
-    }
+        currentUserService.token = data.auth_token;
+        currentUserService.id = data.id;
+      });
+    }; //--End of login function
+  this.logout = function(user){
+    return  $http({method: 'POST', url: '/api/logout', headers: {'Authorization' : user.token}});
+    //--End of return statement
+  };// --End of logout function
 });
 
-app.service('needService', function($http){
-  this.login = function(user){
-    return  $http({method: 'POST', url: '/api', headers: {'X-API-EMAIL' : user.email, 'X-API-PASS' : user.password}})
-      .success( function( data )
-      {
-        success(data);
-      })
-      .error( function( data)
-      {
-      error(data);
-      }); //--End of return statement
-    }
+app.service('currentUserService', function(){
+  this.id = null;
+  this.token = null;
 });
+
+// app.service('needService', function($http){
+//   this.getNeeds = function(user){
+//     return  $http({method: 'POST', url: '/api', headers: {'X-API-EMAIL' : user.email, 'X-API-PASS' : user.password}})
+//       .success( function( data )
+//       {
+//         success(data);
+//       })
+//       .error( function( data)
+//       {
+//       error(data);
+//       }); //--End of return statement
+//     }
+// });
