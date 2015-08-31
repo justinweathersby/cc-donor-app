b8eed224f51857cab794a8754e4edae0d0414910
justinweathersby <justinweathersby@gmail.com>
2015-08-31 01:00:39 -0400

    Added google-place component

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,7 @@
 var app = angular.module('cc-donor-app', ['ionic',
                                           'ngResource',
                                           'ion-autocomplete',
+                                          'ion-google-place',
                                           'ngCordova'])
   .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
