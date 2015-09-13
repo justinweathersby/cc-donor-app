3f22c34f14c6f7f9558ee56e5d372a759fb131da
justinweathersby <justinweathersby@gmail.com>
2015-09-12 22:03:08 -0400

    Minor changes still not working in testing

--- www/js/constants.js
+++ www/js/constants.js
@@ -10,11 +10,11 @@ app.constant('USER_ROLES', {
 });
 
 //-- Staging API Url
-// app.constant('CHATTER_API', {
-//     url:'http://staging.creativechatter.com/api'
-// });
+ app.constant('CHATTER_API', {
+     url:'http://staging.creativechatter.com/api'
+ });
 
-//-- Local Testing API Url
-app.constant('CHATTER_API', {
-    url:'api'
-});
+////-- Local Testing API Url
+//app.constant('CHATTER_API', {
+//    url:'api'
+//});
