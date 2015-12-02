1734f114651a92fe9f50becfff930aeb969dac85
Esau Delke <David@Esaus-MacBook-Pro.local>
2015-12-01 22:07:43 -0500

    font-size

--- www/js/controllers/signup.js
+++ www/js/controllers/signup.js
@@ -1,19 +1,27 @@
 
 
-app.controller('SignController', function($scope, $http) {
-
-
-$scope.signup = function(user)
+app.controller('SignController', function($scope,
+                                                $state, $stateParams,
+                                                $ionicPopup)
 {
 
-	console.log(user);
-	// check if the username is already taken 
-
-	// if not register user and send to tabs.dashboard
-	// save current user in localstorage
-
-}
-
-
+  $scope.createUser = function(user)
+  { //create a new user. Issues a POST to /api/users
+    $scope.user.$save()
+      .then(function(resp) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Success',
+          template: "Thank you for signing up with Creative Chatter. Happy Donating!"
+        });
+        $state.go('login'); // on success go to dashboard.
+      })
+      .catch(function(resp){
+        console.log("Error Response: ", resp.data)
+        var alertPopup = $ionicPopup.alert({
+          title: 'Failed',
+          // template: "Sorry something went wrong. If this problem continues please contact Creative Chatter at support@creativechatter.com"
+        });
+      });
+  };
 
 });
